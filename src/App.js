e788711bdd49079079fc5d1bba7a073b7dba6523
mahdi85lovex94 <mahdi.funlife@gmail.com>
2023-11-28 20:52:07 +0330

    feature - Make Routing v6 🔀

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { ThemeProvider } from "@emotion/react";
 import { useSelector } from "react-redux";
-
-import themeConfigs from "./config/theme.configs";
 import { ToastContainer } from "react-toastify";
 import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import themeConfigs from "./config/theme.configs";
+import MainLayout from "./components/layout/MainLayout";
+import routes from "./routes/routes";
+import PageWrapper from "./components/common/PageWrapper";
 
 function App() {
   const { themeMode } = useSelector((state) => state.themeMode);
@@ -20,10 +24,46 @@ function App() {
         pauseOnHover
         theme={themeMode}
       />
-      <CssBaseline/>
+      <CssBaseline />
 
-
-
+      {/*  app routes */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            {routes.map((route, index) =>
+              route.index ? (
+                <Route
+                  index
+                  key={index}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              ) : (
+                <Route
+                  path={route.path}
+                  key={index}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              )
+            )}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
